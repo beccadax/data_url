24a4563acfa4f2f60e6226c9ca329843788019b5
Brent Royal-Gordon <brent@architechies.com>
2014-03-30 12:54:01 -0700

    Use cleaner syntax

--- spec/create_spec.rb
+++ spec/create_spec.rb
@@ -7,36 +7,36 @@ describe DataURL do
     end
     
     it "should return empty data if data is empty" do
-      expect(DataURL.create('')).to match(/,\Z/)
+      DataURL.create('').should match(/,\Z/)
     end
     
     it "should return data URLs" do
-      expect(DataURL.create('')).to match(/\Adata:/)
+      DataURL.create('').should match(/\Adata:/)
     end
     
     it "should encode as Base64 by default" do
-      expect(DataURL.create('')).to match(/;base64,/)
+      DataURL.create('').should match(/;base64,/)
     end
     
     it 'should URL-encode when Base64 is off' do
-      expect(DataURL.create('abc ', 'application/octet-stream', false)).to match(/abc%20\Z/)
+      DataURL.create('abc ', 'application/octet-stream', false).should match(/abc%20\Z/)
     end
     
     it "should correctly encode Base64" do
-      expect(DataURL.create('abc')).to match(/,YWJj\Z/)
+      DataURL.create('abc').should match(/,YWJj\Z/)
     end
     
     it 'should include content type when specified' do
-      expect(DataURL.create('abc', 'foo/bar')).to match(/:foo\/bar;/)
+      DataURL.create('abc', 'foo/bar').should match(/:foo\/bar;/)
     end
     
     it 'should default content type to octet-stream' do
-      expect(DataURL.create('abc')).to match(/:application\/octet-stream/)
+      DataURL.create('abc').should match(/:application\/octet-stream/)
     end
     
     it 'should not include newlines in Base64 data' do
       data = 'x' * 1000
-      expect(DataURL.create(data)).not_to match(/\n/)
+      DataURL.create(data).should_not match(/\n/)
     end
   end
 end
